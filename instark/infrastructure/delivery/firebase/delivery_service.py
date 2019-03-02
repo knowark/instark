69e77ef7706a58e21d5758168af56cb0ede77c95
jbuitron <jbuitron@servagro.com.co>
2019-03-02 10:14:38 -0500

    Modify notifiaction web

--- instark/infrastructure/delivery/firebase/delivery_service.py
+++ instark/infrastructure/delivery/firebase/delivery_service.py
@@ -1,6 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials, messaging
+# import for create Android notification
 from firebase_admin.messaging import AndroidConfig, AndroidNotification
+# import for create Web notification
+from firebase_admin.messaging import WebpushConfig, WebpushNotification
 from ....application.services import DeliveryService
 
 
@@ -13,9 +16,12 @@ class FirebaseDeliveryService(DeliveryService):
         notification = messaging.Notification(body=content)
         android_notification = AndroidNotification(sound='default')
         android_config = AndroidConfig(notification=android_notification)
+        web_notification = WebpushNotification(sound='default')
+        web_configuration = WebpushConfig(notification=web_configuration)
         message = messaging.Message(
             notification=notification,
             android=android_config,
+            web=web_configuration,
             token=locator)
         return messaging.send(message)
 
@@ -23,9 +29,12 @@ class FirebaseDeliveryService(DeliveryService):
         notification = messaging.Notification(body=content)
         android_notification = AndroidNotification(sound='default')
         android_config = AndroidConfig(notification=android_notification)
+        web_notification = WebpushNotification(sound='default')
+        web_configuration = WebpushConfig(notification=web_configuration)
         message = messaging.Message(
             notification=notification,
             android=android_config,
+            web=web_configuration,
             topic=code)
         return messaging.send(message)
 
