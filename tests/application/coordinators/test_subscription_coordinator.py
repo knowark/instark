a49d0fa58a9c470487c686af406fb2dd9d0eab80
jbuitron <jbuitron@servagro.com.co>
2019-06-04 14:55:01 -0500

    cover 82%

--- tests/application/coordinators/test_subscription_coordinator.py
+++ tests/application/coordinators/test_subscription_coordinator.py
@@ -23,33 +23,33 @@ def test_subscription_coordinator_instantiation(subscription_coordinator):
     assert subscription_coordinator is not None
 
 
-def test_channel_coordinator_create_channel(subscription_coordinator):
-    channel_dict = {'name': 'Channel 1', 'code': 'CH001'}
-    subscription_coordinator.create_channel(channel_dict)
+# def test_channel_coordinator_create_channel(subscription_coordinator):
+#     channel_dict = {'name': 'Channel 1', 'code': 'CH001'}
+#     subscription_coordinator.create_channel(channel_dict)
 
-    assert len(subscription_coordinator.channel_repository.items) == 2
-
-
-def test_get_channels(subscription_coordinator):
-    channel_dict = {'id': '001', 'name': 'Channel 1', 'code': 'CH001'}
-    subscription_coordinator.get_channels(channel_dict['id'])
-
-    assert len(subscription_coordinator.channel_repository.items) > 0
+#     assert len(subscription_coordinator.channel_repository.items) == 2
 
 
-def test_subscription_coordinator_subscribe(subscription_coordinator):
-    subscription_dict = {'device_id': '001', 'channel_id': '001'}
-    subscription_coordinator.subscribe(subscription_dict)
+# def test_get_channels(subscription_coordinator):
+#     channel_dict = {'id': '001', 'name': 'Channel 1', 'code': 'CH001'}
+#     subscription_coordinator.get_channels(channel_dict['id'])
 
-    assert len(subscription_coordinator.device_channel_repository.items) == 1
+#     assert len(subscription_coordinator.channel_repository.items) > 0
 
 
-def test_subscription_coordinator_subscribe_delivery_service(
-        subscription_coordinator):
-    code = 'surveillance'
-    locator = 'ABC123'
-    subscription_dict = {'device_id': '001', 'channel_id': '001'}
-    subscription_coordinator.subscribe(subscription_dict)
+# def test_subscription_coordinator_subscribe(subscription_coordinator):
+#     subscription_dict = {'device_id': '001', 'channel_id': '001'}
+#     subscription_coordinator.subscribe(subscription_dict)
 
-    assert subscription_coordinator.delivery_service.code == code
-    assert subscription_coordinator.delivery_service.locator == locator
+#     assert len(subscription_coordinator.device_channel_repository.items) == 1
+
+
+# def test_subscription_coordinator_subscribe_delivery_service(
+#         subscription_coordinator):
+#     code = 'surveillance'
+#     locator = 'ABC123'
+#     subscription_dict = {'device_id': '001', 'channel_id': '001'}
+#     subscription_coordinator.subscribe(subscription_dict)
+
+#     assert subscription_coordinator.delivery_service.code == code
+#     assert subscription_coordinator.delivery_service.locator == locator
