cdbbb8e16504d73cf3ba5dcd9f1b57c1a5597b2c
jjalvarez <jjalvarez@servagro.com.co>
2019-09-09 10:52:42 -0500

    Testing registry.

--- tests/infrastructure/core/config/test_context.py
+++ tests/infrastructure/core/config/test_context.py
@@ -2,7 +2,7 @@ from instark.infrastructure.core.configuration import (
     TrialConfig, MemoryRegistry, Context)
 
 
-def test_context_instantiation():
+def test_memory_registry_context_instantiation():
     config = TrialConfig()
     registry = MemoryRegistry(config)
     context = Context(config, registry)
--- tests/infrastructure/core/config/test_registry.py
+++ tests/infrastructure/core/config/test_registry.py
@@ -1,2 +1,27 @@
+from pytest import fixture
+from json import dump
 from instark.infrastructure.core.configuration import (
-    TrialConfig, DevelopmentConfig, MemoryRegistry)
+    ProductionConfig, ProductionRegistry, Context)
+from instark.infrastructure.core.configuration import \
+    registry as registry_module
+
+
+def test_production_registry_context_instantiation(monkeypatch):
+
+    firebase_delivery_service = False
+
+    class MockFirebaseDeliveryService():
+        def __init__(self, config):
+            nonlocal firebase_delivery_service
+            firebase_delivery_service = True
+
+    monkeypatch.setattr(
+        registry_module, "FirebaseDeliveryService",
+        MockFirebaseDeliveryService)
+
+    production_config = ProductionConfig()
+
+    registry = ProductionRegistry(production_config)
+    context = Context(production_config, registry)
+    assert context.config == production_config
+    assert context.registry == registry
