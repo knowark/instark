8f9fb1cc7a578f54f3509aab2cd452427f0ec6b9
jjalvarez <jjalvarez@servagro.com.co>
2019-09-09 16:26:49 -0500

    improving typing and imports

--- instark/infrastructure/core/crypto/jwt_supplier.py
+++ instark/infrastructure/core/crypto/jwt_supplier.py
@@ -6,7 +6,7 @@ class JwtSupplier:
     def __init__(self, secret: str) -> None:
         self.secret = secret
 
-    def decode(self, token: str, secret=None, verify=True):
+    def decode(self, token: str, secret=None, verify=True) -> dict:
         secret = secret or self.secret
         return jwt.decode(token, secret, verify=verify,
                           algorithms=['HS256'])
--- instark/infrastructure/web/__init__.py
+++ instark/infrastructure/web/__init__.py
@@ -2,4 +2,4 @@ from gevent import monkey
 monkey.patch_all()  # noqa
 from .base import create_app
 from .server import ServerApplication
-from .hooks import *
+from .hooks import register_error_handler
